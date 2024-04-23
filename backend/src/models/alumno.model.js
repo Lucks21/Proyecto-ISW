320799655606e9f13d5325d34cdc2555b069cd41
Lucks21 <manobazete21@gmail.com>
2024-04-22 23:14:22 -0400

    modelo alumno

---
+++ backend/src/models/alumno.model.js
@@ -0,0 +1,33 @@
+"use strict";
+// Import the 'mongoose' module to create the database connection
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const alumnoSchema = new Schema({
+        rut: { 
+            type: String, 
+            unique: true, 
+            required: true 
+        },
+        contraseña: { 
+            type: String, 
+            required: true 
+        },
+        nombre: { 
+            type: String 
+        },
+        correoElectronico: { 
+            type: String 
+        },
+        prestamosActivos: [{ 
+            type: Schema.Types.ObjectId, 
+            ref: 'Reserva' 
+        }],
+        historialPrestamos: [{
+            type: Schema.Types.ObjectId, 
+            ref: 'Reserva' 
+        }]
+    } 
+);
+const Alumno = mongoose.model("alumno",dañoSchema);
+export default Alumno;
