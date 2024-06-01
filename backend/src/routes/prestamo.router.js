3b44d463bd6c5a3f7baf20e8dce95c903b0910b2
Lucks21 <manobazete21@gmail.com>
2024-06-01 09:59:01 -0400

    wip

--- backend/src/routes/prestamo.router.js
+++ backend/src/routes/prestamo.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const prestamoController = require('../controllers/prestamo.controller');
 const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 
-router.get('/prestamos/activos', authenticationMiddleware, prestamoController.getPréstamosActivos);
-router.get('/usuarios/:userId/prestamos', authenticationMiddleware, prestamoController.getHistorialPréstamos);
+router.get('/prestamos/activos', authenticationMiddleware, prestamo.controller.getPréstamosActivos);
+router.get('/usuarios/:userId/prestamos', authenticationMiddleware, prestamo.controller.getHistorialPréstamos);
 
 module.exports = router;
