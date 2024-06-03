eca182729ebcdcbc3ba22dcc2317b30af52b1c6d
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-06-02 23:24:32 -0400

    Update2

--- backend/src/routes/notificacion.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const notificacionController = require('../controllers/notificacion.controller.js');
-
-const router = Router();
-
-router.post('/solicitar-notificacion', notificacionController.solicitarNotificacion);
-
-module.exports = router;
