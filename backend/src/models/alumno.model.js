9d65f5abebe8a60e36b14ea4d7c2ac51d06ecfa6
Lucks21 <manobazete21@gmail.com>
2024-07-07 20:38:49 -0400

    update models

---
+++ backend/src/models/alumno.model.js
@@ -0,0 +1,45 @@
+"use strict";
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const alumnoSchema = new Schema(
+  {
+    rut: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    contraseña: {
+      type: String,
+      required: true,
+    },
+    nombre: {
+      type: String,
+      required: true,
+    },
+    correoElectronico: {
+      type: String,
+      required: true,
+    },
+    reservasActivas: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reserva",
+      },
+    ],
+    historialReservas: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reserva",
+      },
+    ],
+  },
+  {
+    versionKey: false, //esto es para desactivar la crecion del campo '_v' en los documentos
+  },
+);
+
+const Alumno = mongoose.model("Alumno", alumnoSchema);
+
+export default Alumno;
---
+++ backend/src/models/reservas.model.js
@@ -0,0 +1,41 @@
+"use strict";
+import mongoose from "mongoose";
+const reservaSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, // esto indica que el campo es obligatorio
+    },
+    implementoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Implementos',
+      required: function() { return !this.instalacionId; }  //solo va a ser requerido si instalacionID no está
+    },
+    instalacionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Instalacion',
+      required: function() { return !this.implementoId; } //lo mismo que con implemento
+    },
+    fechaInicio: {
+      type: Date,
+      required: true,
+    },
+    fechaFin: {
+      type: Date,
+    },
+    estado: {
+      //activo: reservaron
+      //inactivo: se termino de usar la instalacion o implemento
+      type: String,
+      enum: ["activo", "no activo"],
+      default: "activo",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false, //esto es para desactivar la crecion del campo '_v' en los documentos
+  },
+);
+const Reserva = mongoose.model("Reserva", reservaSchema);
+export default Reserva;
