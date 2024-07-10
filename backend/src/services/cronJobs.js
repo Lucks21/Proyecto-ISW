502096b673d6b7c2131e4570f36098a068c47794
Lucks21 <manobazete21@gmail.com>
2024-07-10 08:51:53 -0400

    test

--- backend/src/services/cronJobs.js
+++ backend/src/services/cronJobs.js
@@ -3,7 +3,7 @@ import ReservaService from './services/reservas.service.js';
 
 console.log('=> Cron job configurado');
 
-// Programar un cron job para ejecutarse cada minuto (puedes ajustar el intervalo según tus necesidades)
+
 cron.schedule('* * * * *', async () => {
   console.log('Cron job ejecutándose: Revisando reservas expiradas...');
   await ReservaService.finalizarReservasExpiradas();
--- backend/src/services/testEmailService.js
+++ backend/src/services/testEmailService.js
@@ -1,4 +1,3 @@
-import sendEmail from './email.services.js';
 import {EMAIL_USER,EMAIL_PASS} from "../config/configEnv.js";
 import nodemailer from 'nodemailer';
 
@@ -12,8 +11,8 @@ const transporter = nodemailer.createTransport({
 
 async function testSendEmail() {
   const info = await transporter.sendMail({
-    from: '"Proyecto ingenieria de software👻" <${EMAIL_USER}>', // sender address
-    to: "anays.mansilla2001@alumnos.ubiobio.cl, mayramansilla869@gmail.com,matias.pereira2001@alumnos.ubiobio.cl,felipepd14@gmail.com", // list of receivers
+    from: '"Proyecto ingenieria de software👻" <${EMAIL_USER}>', 
+    to: ",matias.pereira2001@alumnos.ubiobio.cl",
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     
