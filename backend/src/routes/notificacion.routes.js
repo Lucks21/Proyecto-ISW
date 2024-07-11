081a876debcb441cc413c6716994c11e303b26f1
Lucks21 <manobazete21@gmail.com>
2024-07-11 08:31:49 -0400

    wip

--- backend/src/routes/notificacion.routes.js
+++ backend/src/routes/notificacion.routes.js
@@ -3,6 +3,8 @@ import notificacionController from '../controllers/notificacion.controller.js';
 
 const router = Router();
 
-router.post('/solicitar', notificacionController.solicitarNotificacion); 
+router.post('/solicitar', notificacionController.solicitarNotificacion);
+router.post('/notificarImplemento', notificacionController.notificarDisponibilidadImplemento);
+router.post('/notificarInstalacion', notificacionController.notificarDisponibilidadInstalacion);
 
 export default router;
