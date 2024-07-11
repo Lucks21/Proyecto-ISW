eb7c02603b50d4ec6f662f900a18648962971528
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-07-11 12:14:59 -0400

    update

--- backend/src/schema/instalacion.schema.js
+++ backend/src/schema/instalacion.schema.js
@@ -1,6 +1,5 @@
 import Joi from 'joi';
 
-// Definición del esquema para el horario
 const horarioSchema = Joi.object({
   dia: Joi.string().valid('lunes', 'martes', 'miércoles', 'jueves', 'viernes').required(),
   inicio: Joi.string().pattern(/^([01]\d|2[0-3]):([0-5]\d)$/).required().messages({
@@ -16,33 +15,29 @@ const crearInstalacionSchema = Joi.object({
     'any.required': 'El nombre es obligatorio',
     'string.pattern.base': 'El nombre solo puede contener letras, números, guiones, guiones bajos y espacios, y debe incluir letras'
   }),
-  descripcion: Joi.string().trim().optional().pattern(/[a-zA-Z]+/).messages({
-    'string.pattern.base': 'La descripción debe contener letras además de números y caracteres especiales'
-  }),
-  capacidad: Joi.number().integer().min(1).required().messages({
-    'any.required': 'La capacidad es obligatoria',
-    'number.min': 'La capacidad debe ser al menos 1',
-    'number.base': 'La capacidad debe ser un número entero'
+  descripcion: Joi.string().trim().optional(),
+  fechaAdquisicion: Joi.date().required().messages({
+    'any.required': 'La fecha de adquisición es obligatoria',
+    'date.base': 'La fecha de adquisición debe ser una fecha válida'
   }),
   horarioDisponibilidad: Joi.array().items(horarioSchema).unique((a, b) => a.dia === b.dia).optional().messages({
     'array.unique': 'No puede haber horarios superpuestos para el mismo día'
-  })
+  }),
+  estado: Joi.string().valid('disponible', 'no disponible').optional()
 });
 
 const actualizarInstalacionSchema = Joi.object({
-  nombre: Joi.string().pattern(/^[a-zA-Z0-9-_ ]+$/).trim().optional().messages({
-    'string.pattern.base': 'El nombre solo puede contener letras, números, guiones, guiones bajos y espacios, y debe incluir letras'
+  nombre: Joi.string().pattern(/^[a-zA-Z0-9-_]+$/).trim().optional().messages({
+    'string.pattern.base': 'El nombre solo puede contener letras, números, guiones y guiones bajos, y debe incluir letras'
   }),
-  descripcion: Joi.string().trim().optional().pattern(/[a-zA-Z]+/).messages({
-    'string.pattern.base': 'La descripción debe contener letras además de números y caracteres especiales'
-  }),
-  capacidad: Joi.number().integer().min(1).optional().messages({
-    'number.min': 'La capacidad debe ser al menos 1',
-    'number.base': 'La capacidad debe ser un número entero'
+  descripcion: Joi.string().trim().optional(),
+  fechaAdquisicion: Joi.date().optional().messages({
+    'date.base': 'La fecha de adquisición debe ser una fecha válida'
   }),
   horarioDisponibilidad: Joi.array().items(horarioSchema).unique((a, b) => a.dia === b.dia).optional().messages({
     'array.unique': 'No puede haber horarios superpuestos para el mismo día'
-  })
+  }),
+  estado: Joi.string().valid('disponible', 'no disponible').optional()
 });
 
 const idSchema = Joi.string().hex().length(24).required().messages({
