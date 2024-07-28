6a8a75ca6b99dd514b623a3f289e07f6bba0ce81
Lucks21 <manobazete21@gmail.com>
2024-07-28 19:34:44 -0400

    update modelo notificaciones

--- backend/src/models/notificaciones.model.js
+++ backend/src/models/notificaciones.model.js
@@ -6,17 +6,17 @@ const { Schema } = mongoose;
 const NotificacionSchema = new Schema({
   recursoId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    required: [true, 'El ID del recurso es obligatorio'],
   },
   recursoTipo: {
     type: String,
     enum: ['implemento', 'instalacion'],
-    required: true,
+    required: [true, 'El tipo de recurso es obligatorio'],
   },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Alumno',
-    required: true,
+    required: [true, 'El ID del usuario es obligatorio'],
   },
 },{
   timestamps: false,
