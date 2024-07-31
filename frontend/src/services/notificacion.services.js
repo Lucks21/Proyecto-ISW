bcc4c2b1bf7cb9623d09bce360b5d103d5db0fe8
Lucks21 <manobazete21@gmail.com>
2024-07-31 17:15:36 -0400

    wip solicitud

--- frontend/src/services/notificacion.services.js
+++ frontend/src/services/notificacion.services.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-
+const API_URL = 'http://localhost:3200/api';
 export const solicitarNotificacion = async (recursoId, recursoTipo, userId) => {
   try {
     const token = localStorage.getItem("accestkn");
     if (!token) return;
 
-    const response = await axios.post("/notificaciones/solicitar", {
+    const response = await axios.post(`${API_URL}/notificaciones/solicitar`, {
       recursoId,
       recursoTipo,
       userId
