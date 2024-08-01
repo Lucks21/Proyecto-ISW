98599c620efccf5db95495ba572cbbfbde1a35ab
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-08-01 06:48:06 -0400

    update

--- frontend/src/components/ModalEditImpl.jsx
+++ frontend/src/components/ModalEditImpl.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { partialUpdateImplemento, getAllImplementos } from '../services/implementos.services';
+import { partialUpdateImplemento } from '../services/implementos.services';
 import { toast } from 'react-toastify';
 import moment from 'moment';
 
-const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
+const ModalEditImpl = ({ implemento, setShowModalEditar, fetchImplementos }) => {
   const [nombre, setNombre] = useState(implemento.nombre || '');
   const [descripcion, setDescripcion] = useState(implemento.descripcion || '');
   const [cantidad, setCantidad] = useState(implemento.cantidad || '');
@@ -12,7 +12,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
   const [horario, setHorario] = useState(implemento.horarioDisponibilidad);
   const [showConfirmSave, setShowConfirmSave] = useState(false);
   const [showConfirmCancel, setShowConfirmCancel] = useState(false);
-  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     setHorario(implemento.horarioDisponibilidad);
@@ -73,18 +72,15 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
     }
 
     try {
+      console.log('Actualizando implemento con los siguientes campos:', updatedFields);
       const response = await partialUpdateImplemento(implemento._id, updatedFields);
-      const implementos = await getAllImplementos();
-      setImplementos(implementos); // Actualiza la lista de implementos en el estado
+      console.log('Respuesta del servidor:', response.data);
+      fetchImplementos();
       setShowModalEditar(false);
       toast.success('Implemento actualizado con éxito');
     } catch (error) {
-      if (error.response && error.response.data) {
-        setErrors(error.response.data.errors || {});
-        toast.error(`Error al actualizar implemento: ${error.response.data.message}`);
-      } else {
-        toast.error('Error al actualizar implemento');
-      }
+      console.error('Error al actualizar implemento:', error.response?.data?.message || error.message);
+      toast.error(`Error al actualizar implemento: ${error.response?.data?.message || error.message}`);
     }
   };
 
@@ -94,7 +90,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div className="bg-[#EFF396] p-6 rounded-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-4">Editar Implemento</h2>
-        {errors.general && <div className="text-red-500 mb-4">{errors.general}</div>}
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Nombre</label>
           <input
@@ -104,7 +99,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
             value={nombre}
             onChange={(e) => setNombre(e.target.value)}
           />
-          {errors.nombre && <div className="text-red-500">{errors.nombre}</div>}
         </div>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Descripción</label>
@@ -115,7 +109,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
             value={descripcion}
             onChange={(e) => setDescripcion(e.target.value)}
           />
-          {errors.descripcion && <div className="text-red-500">{errors.descripcion}</div>}
         </div>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Cantidad</label>
@@ -126,7 +119,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
             value={cantidad}
             onChange={(e) => setCantidad(e.target.value)}
           />
-          {errors.cantidad && <div className="text-red-500">{errors.cantidad}</div>}
         </div>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Estado</label>
@@ -138,7 +130,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
             <option value="disponible">Disponible</option>
             <option value="no disponible">No disponible</option>
           </select>
-          {errors.estado && <div className="text-red-500">{errors.estado}</div>}
         </div>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Días de Disponibilidad:</label>
@@ -156,7 +147,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
                 <option value="jueves">Jueves</option>
                 <option value="viernes">Viernes</option>
               </select>
-              {errors[`horarioDisponibilidad.${index}.dia`] && <div className="text-red-500">{errors[`horarioDisponibilidad.${index}.dia`]}</div>}
               <select
                 className="mt-1 block w-1/3 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
                 value={horario[index]?.inicio || ''}
@@ -167,7 +157,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
                   <option key={hora} value={hora}>{hora}</option>
                 ))}
               </select>
-              {errors[`horarioDisponibilidad.${index}.inicio`] && <div className="text-red-500">{errors[`horarioDisponibilidad.${index}.inicio`]}</div>}
               <select
                 className="mt-1 block w-1/3 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
                 value={horario[index]?.fin || ''}
@@ -178,7 +167,6 @@ const ModalEditImpl = ({ implemento, setShowModalEditar, setImplementos }) => {
                   <option key={hora} value={hora}>{hora}</option>
                 ))}
               </select>
-              {errors[`horarioDisponibilidad.${index}.fin`] && <div className="text-red-500">{errors[`horarioDisponibilidad.${index}.fin`]}</div>}
               <button
                 type="button"
                 className="text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2.5 py-1.5"
