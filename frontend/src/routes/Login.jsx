a385f8bc409d0c00325a2fc485879a3bfa8aed32
Lucks21 <manobazete21@gmail.com>
2024-08-01 00:20:58 -0400

    Revert "wip"
    This reverts commit 1dc822d85745c91ab2ed8a58d168834e4e59d03c.

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,30 +1,18 @@
-import React from 'react';
 import LoginForm from '../components/LoginForm';
 import { useNavigate } from 'react-router-dom';
 import bannerImage from '../assets/ubb_logo_inicio.png';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
   const navigate = useNavigate();
 
-  const showAlert = (type, message) => {
-    if (type === 'success') {
-      toast.success(message);
-    } else {
-      toast.error(message);
-    }
-  };
-
   if (localStorage.getItem('user')) {
     return (
       <>
-        <h2>Ya estás logeado!</h2>
+        <h2>Ya estas logeado!</h2>
         <button onClick={() => navigate('/')}>Ir a home</button>
       </>
     );
   }
-
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-light-yellow">
       <div className="header-footer-bg w-full"></div>
@@ -36,11 +24,10 @@ function Login() {
           style={{ transform: 'translateX(-35px)' }}
         />
         <div className="w-full max-w-md">
-          <LoginForm showAlert={showAlert} />
+          <LoginForm />
         </div>
       </div>
       <div className="header-footer-bg w-full"></div>
-      <ToastContainer />
     </div>
   );
 }
