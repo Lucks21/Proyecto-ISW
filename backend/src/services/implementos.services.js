899c351007945017d9b103382a6f487d1a5d06c6
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-08-01 09:36:55 -0400

    Revert "servicio2"

--- backend/src/services/implementos.services.js
+++ backend/src/services/implementos.services.js
@@ -69,6 +69,7 @@ const validarHorarios = (horarioDisponibilidad) => {
   }
 };
 
+
 // Función para normalizar la fecha
 const normalizarFecha = (fecha) => {
   let parsedDate = parse(fecha, 'dd-MM-yyyy', new Date());
@@ -240,7 +241,6 @@ export const actualizarImplementoParcial = async (id, camposActualizados) => {
     throw new Error(error.message || 'Error interno del servidor');
   }
 };
-
 // Servicio para eliminar un implemento
 export const eliminarImplemento = async (id) => {
   const implementoEliminado = await Implemento.findByIdAndDelete(id);
@@ -268,7 +268,6 @@ export const obtenerHistorialImplemento = async (id) => {
 
   return { message: 'Historial obtenido con éxito.', data: historial };
 };
-
 export const obtenerImplementoPorNombre = async (nombre) => {
   const implemento = await Implemento.findOne({ nombre });
   if (!implemento) {
