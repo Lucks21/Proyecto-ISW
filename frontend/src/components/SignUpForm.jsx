eb7ec228792cf06d2248bc5ccb68459afeb15041
Lucks21 <manobazete21@gmail.com>
2024-08-01 12:28:04 -0400

    registrar prueba

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -36,7 +36,7 @@ function SignUp() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Juanito Perez"
             required
-            {...register("name", { required: true })}
+            {...register("name", { required: false })}
           />
         </div>
         <div>
